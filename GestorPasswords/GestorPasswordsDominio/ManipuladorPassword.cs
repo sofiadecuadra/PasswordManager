63eb5afa8d3cf8034fa4cca72f41937ebe9e8774
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-04-18 15:40:36 -0300

    Pass PasswordConLargoEntre8Y14Caracteres Unit Test

--- GestorPasswords/GestorPasswordsDominio/ManipuladorPassword.cs
+++ GestorPasswords/GestorPasswordsDominio/ManipuladorPassword.cs
@@ -10,12 +10,16 @@ namespace GestorPasswordsDominio
     {
         public static TipoFortaleza FortalezaDePassword(String passwordToCheck)
         {
+            if (passwordToCheck.Length < 8) return TipoFortaleza.Rojo;
+            if (passwordToCheck.Length >= 8 && passwordToCheck.Length <= 14) return TipoFortaleza.Naranja;
+
             return TipoFortaleza.Rojo;
         }
     }
 
     public enum TipoFortaleza
     {
-        Rojo = 0
+        Rojo = 0,
+        Naranja = 1
     }
 }
