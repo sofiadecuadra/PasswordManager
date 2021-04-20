cc27ab4cba785e362c9fe9e155447c60f4d9836b
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-04-20 20:44:03 -0300

    Refactor Length > 25 Unit test

---
+++ GestorPasswords/GestorPasswordsDominio/PasswordHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GestorPasswordsDominio
+{
+    public class PasswordHandler
+    {
+        public static PasswordStrengthType PasswordStrength(String passwordToCheck)
+        {
+            if (ContainsLessThan8Characters(passwordToCheck)) return PasswordStrengthType.Red;
+            if (ContainsBetween8And14Characters(passwordToCheck)) return PasswordStrengthType.Orange;
+            if (ContainsJustUpperAndLowerCaseSymbolsAndNumbers(passwordToCheck)) return PasswordStrengthType.DarkGreen;
+
+            bool isLightGreen = ContainsJustUpperAndLowerCase(passwordToCheck) || ContainsJustUpperAndLowerCaseAndSymbolsOrNumbers(passwordToCheck);
+            if (isLightGreen) return PasswordStrengthType.LightGreen;
+            bool isYellow = ContainsJustUpperOrLowerCase(passwordToCheck)
+                            || ContainsJustSymbolsAndNumbers(passwordToCheck)
+                            || ContainsJustUpperOrLowerCaseAndSymbolsOrNumbers(passwordToCheck);
+            if (isYellow) return PasswordStrengthType.Yellow;
+
+            throw new IncorrectLengthException($"Length should be between 8 and 25 characters but is: ${passwordToCheck.Length}");
+        }
+
+        private static bool ContainsJustUpperOrLowerCaseAndSymbolsOrNumbers(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"(?=^.{14,}$)((?=.*((\d)|(\W))+))(?![.\n])(?=.*([A-Z])|([a-z])).*$");
+        }
+
+        private static bool ContainsJustSymbolsAndNumbers(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"(?=^.{14,}$)((?=.*\d)(?=.*\W+))(?![.\n]).*$");
+        }
+
+        private static bool ContainsJustUpperAndLowerCaseAndSymbolsOrNumbers(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"(?=^.{14,}$)((?=.*((\d)|(\W))+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+        }
+
+        private static bool ContainsJustUpperAndLowerCaseSymbolsAndNumbers(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"(?=^.{14,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+        }
+
+        private static bool ContainsJustUpperAndLowerCase(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"^(?=[a-z]+[A-Z]+|[A-Z]+[a-z]+)[a-zA-Z]{14,25}$");
+        }
+
+        private static bool ContainsJustUpperOrLowerCase(string passwordToCheck)
+        {
+            return Regex.IsMatch(passwordToCheck, @"^[A-Za-z\s]+$");
+        }
+
+        private static bool ContainsLessThan8Characters(string passwordToCheck)
+        {
+            return passwordToCheck.Length < 8;
+        }
+
+        private static bool ContainsBetween8And14Characters(string passwordToCheck)
+        {
+            return passwordToCheck.Length >= 8 && passwordToCheck.Length <= 14;
+        }
+    }
+
+    public enum PasswordStrengthType
+    {
+        Red = 0,
+        Orange = 1,
+        Yellow = 2,
+        LightGreen = 3,
+        DarkGreen = 4
+    }
+}
--- GestorPasswords/GestorPasswordsTest/ManipuladorPasswordTest.cs
+++ GestorPasswords/GestorPasswordsTest/ManipuladorPasswordTest.cs
@@ -10,83 +10,83 @@ namespace GestorPasswordsTest
         [TestMethod]
         public void EsPasswordDebil()
         {
-            Assert.AreEqual(TipoFortaleza.Rojo, ManipuladorPassword.PasswordStrength("hola"));
+            Assert.AreEqual(PasswordStrengthType.Red, PasswordHandler.PasswordStrength("hola"));
         }
 
         [TestMethod]
         public void PasswordConLargoEntre8Y14Caracteres()
         {
-            Assert.AreEqual(TipoFortaleza.Naranja, ManipuladorPassword.PasswordStrength("holamundoesto"));
+            Assert.AreEqual(PasswordStrengthType.Orange, PasswordHandler.PasswordStrength("holamundoesto"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMinusculas()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("holamundoestoesunapass"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("holamundoestoesunapass"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculas()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("AHHHHHHHHHHHHHHH"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("AHHHHHHHHHHHHHHH"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasYMinusculas()
         {
-            Assert.AreEqual(TipoFortaleza.VerdeClaro, ManipuladorPassword.PasswordStrength("holaMundoEstoEsUnaPass"));
+            Assert.AreEqual(PasswordStrengthType.LightGreen, PasswordHandler.PasswordStrength("holaMundoEstoEsUnaPass"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasMinusculasNumerosYSimbolos()
         {
-            Assert.AreEqual(TipoFortaleza.VerdeOscuro, ManipuladorPassword.PasswordStrength("holaMundoEstoEs1234-+@"));
+            Assert.AreEqual(PasswordStrengthType.DarkGreen, PasswordHandler.PasswordStrength("holaMundoEstoEs1234-+@"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasMinusculasYSimbolos()
         {
-            Assert.AreEqual(TipoFortaleza.VerdeClaro, ManipuladorPassword.PasswordStrength("holaMundoEstoEs-+@"));
+            Assert.AreEqual(PasswordStrengthType.LightGreen, PasswordHandler.PasswordStrength("holaMundoEstoEs-+@"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasMinusculasYNumeros()
         {
-            Assert.AreEqual(TipoFortaleza.VerdeClaro, ManipuladorPassword.PasswordStrength("holaMundoEstoEs1234"));
+            Assert.AreEqual(PasswordStrengthType.LightGreen, PasswordHandler.PasswordStrength("holaMundoEstoEs1234"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloNumerosYSimbolos()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("123456789@!#$%^"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("123456789@!#$%^"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasYSimbolos()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("HOLAESTOSONMAYUS@!#$%^"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("HOLAESTOSONMAYUS@!#$%^"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMinusculasYSimbolos()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("holaestosonminus@!#$%^"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("holaestosonminus@!#$%^"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMayusculasYNumeros()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("HOLAESTOSONMAYUS12345"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("HOLAESTOSONMAYUS12345"));
         }
 
         [TestMethod]
         public void PasswordConLargoMayorA14CaracteresSoloMinusculasYNumeros()
         {
-            Assert.AreEqual(TipoFortaleza.Amarillo, ManipuladorPassword.PasswordStrength("holaestosonminus12345"));
+            Assert.AreEqual(PasswordStrengthType.Yellow, PasswordHandler.PasswordStrength("holaestosonminus12345"));
         }
 
         [TestMethod]
         [ExpectedException(typeof(IncorrectLengthException))]
-        public void PasswordConLargoMayorA25Caracteres() => ManipuladorPassword.PasswordStrength("012345678901234567890123456");
+        public void PasswordConLargoMayorA25Caracteres() => PasswordHandler.PasswordStrength("012345678901234567890123456");
     }
 }
