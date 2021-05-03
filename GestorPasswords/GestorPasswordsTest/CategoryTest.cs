47daa7c5178d6412e0e78dd8969e65f1d9ba4411
sofidecuadra <sofidecuadra@gmail.com>
2021-05-02 23:27:35 -0300

    Red, Green and Refactor ModifyNotesOfCreditCardToAValidOne

--- GestorPasswords/GestorPasswordsTest/CategoryTest.cs
+++ GestorPasswords/GestorPasswordsTest/CategoryTest.cs
@@ -656,6 +656,36 @@ namespace GestorPasswordsTest
             Assert.IsTrue(aCategory.ModifyCreditCard(aCreditCard, newCreditCard));
         }
 
+        [TestMethod]
+        public void ModifyNotesOfCreditCardToAValidOne()
+        {
+            CreditCard aCreditCard = new CreditCard()
+            {
+                Number = "1234567891234567",
+                Type = "Visa",
+                Name = "Visa Gold",
+                Code = "234",
+                Notes = "",
+                ExpirationDate = new DateTime(2023, 12, 25),
+                Category = aCategory,
+            };
+
+            aCategory.AddCreditCard(aCreditCard);
+
+            CreditCard newCreditCard = new CreditCard()
+            {
+                Number = "1234567891234567",
+                Type = "Visa",
+                Name = "Visa Gold",
+                Code = "234",
+                Notes = "aNote",
+                ExpirationDate = new DateTime(2023, 12, 25),
+                Category = aCategory,
+            };
+
+            Assert.IsTrue(aCategory.ModifyCreditCard(aCreditCard, newCreditCard));
+        }
+
 
         [TestMethod]
         public void AddValidUserPasswordPair()
