cfb69aa01b6bba96df56993e15e0a8cc3afb2f68
Fernanda Secinaro <secinarof@gmail.com>
2021-05-04 15:17:54 -0300

    red, green and refactor for ModifyRedUserPasswordPairToALightGreenOne

--- GestorPasswords/GestorPasswordsTest/CategoryTest.cs
+++ GestorPasswords/GestorPasswordsTest/CategoryTest.cs
@@ -1086,6 +1086,34 @@ namespace GestorPasswordsTest
         }
 
         [TestMethod]
+        public void ModifyRedUserPasswordPairToALightGreenOne()
+        {
+            UserPasswordPair aUserPasswordPair = new UserPasswordPair()
+            {
+                Password = "myPass",
+                Notes = "myNotes",
+                Username = "myUsername",
+                Site = "mySite",
+                Category = aCategory,
+            };
+
+            aCategory.AddUserPasswordPair(aUserPasswordPair);
+
+            UserPasswordPair newUserPasswordPair = new UserPasswordPair()
+            {
+                Password = "newPassword12345",
+                Notes = "myNotes",
+                Username = "myUsername",
+                Site = "mySite",
+                Category = aCategory,
+            };
+
+            Assert.IsTrue(aCategory.ModifyUserPasswordPair(aUserPasswordPair, newUserPasswordPair));
+            Assert.AreEqual(0, aCategory.User.GetRedUserPasswordPairs().Length);
+            Assert.AreEqual(1, aCategory.User.GetLightGreenUserPasswordPairs().Length);
+        }
+
+        [TestMethod]
         public void ModifyCategoryOfUserPasswordPairToAValidOneAndNotChangingPassword()
         {
             UserPasswordPair aUserPasswordPair = new UserPasswordPair()
