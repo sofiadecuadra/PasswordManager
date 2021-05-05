da2a6dfd8bb29bf1b0bdceb2f2a318ad3ae28835
Fernanda Secinaro <secinarof@gmail.com>
2021-05-04 22:40:36 -0300

    upgrade of category coverage to 100%

--- GestorPasswords/GestorPasswordsDominio/Category.cs
+++ GestorPasswords/GestorPasswordsDominio/Category.cs
@@ -54,6 +54,16 @@ namespace GestorPasswordsDominio
 
         private bool CreditCardIsValid(CreditCard aCreditCard)
         {
+            if (CreditCardNumberAlreadyExistsInUser(aCreditCard.Number))
+            {
+                throw new ExceptionCreditCardNumberAlreadyExistsInUser("The credit card number already exists in user");
+            }
+
+            return CreditCardDataIsValid(aCreditCard);
+        }
+
+        public bool CreditCardDataIsValid(CreditCard aCreditCard)
+        {
             if (!CreditCardContainsOnlyDigits(aCreditCard.Number))
             {
                 throw new ExceptionCreditCardDoesNotContainOnlyDigits("The credit card number must only contain digits");
@@ -81,10 +91,6 @@ namespace GestorPasswordsDominio
             if (!notesHaveValidLength(aCreditCard.Notes))
             {
                 throw new ExceptionCreditCardHasInvalidNotesLength("The notes' length must be up to 250, but it's current length is " + aCreditCard.Notes.Length);
-            }
-            if (CreditCardNumberAlreadyExistsInUser(aCreditCard.Number))
-            {
-                throw new ExceptionCreditCardNumberAlreadyExistsInUser("The credit card number already exists in user");
             }
 
             return true;
@@ -133,41 +139,12 @@ namespace GestorPasswordsDominio
 
         private bool newCreditCardIsValid(CreditCard oldCreditCard, CreditCard newCreditCard)
         {
-            if (!CreditCardContainsOnlyDigits(newCreditCard.Number))
-            {
-                throw new ExceptionCreditCardDoesNotContainOnlyDigits("The credit card number must only contain digits");
-            }
-            if (!CreditCardNumberHasValidLength(newCreditCard.Number))
-            {
-                throw new ExceptionCreditCardHasInvalidNumberLength("The credit card number must contain 16 digits, but currently it has " + newCreditCard.Number.Length);
-            }
-            if (!LengthBetween3And25(newCreditCard.Type))
-            {
-                throw new ExceptionCreditCardHasInvalidTypeLength("The type's length must be between 3 and 25, but it's current length is " + newCreditCard.Type.Length);
-            }
-            if (!LengthBetween3And25(newCreditCard.Name))
-            {
-                throw new ExceptionCreditCardHasInvalidNameLength("The name's length must be between 3 and 25, but it's current length is " + newCreditCard.Name.Length);
-            }
-            if (!codeHasValidLength(newCreditCard.Code))
-            {
-                throw new ExceptionCreditCardHasInvalidCodeLength("The code's length must be between 3 and 4, but it's current length is " + newCreditCard.Code.Length);
-            }
-            if (!codeContainNumericCharactersOnly(newCreditCard.Code))
-            {
-                throw new ExceptionCreditCardCodeHasNonNumericCharacters("The code should contain numeric characters only but is " + newCreditCard.Code);
-            }
-            if (!notesHaveValidLength(newCreditCard.Notes))
-            {
-                throw new ExceptionCreditCardHasInvalidNotesLength("The notes' length must be up to 250, but it's current length is " + newCreditCard.Notes.Length);
-            }
-
             if (!CreditCardNumbersAreEqual(oldCreditCard.Number, newCreditCard.Number) && CreditCardNumberAlreadyExistsInUser(newCreditCard.Number))
             {
                 throw new ExceptionCreditCardNumberAlreadyExistsInUser("The credit card number already exists in user");
             }
 
-            return true;
+            return CreditCardDataIsValid(newCreditCard);
         }
 
         private bool CreditCardNumbersAreEqual (string oldCreditCardNumber, string newCreditCardNumber)
