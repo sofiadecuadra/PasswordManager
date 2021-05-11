4ea829b175db1ca66590902910db8d720f5f998a
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-05-11 09:46:44 -0300

    Refactor CreditCardTest

--- GestorPasswords/GestorPasswordsTest/CreditCardTest.cs
+++ GestorPasswords/GestorPasswordsTest/CreditCardTest.cs
@@ -9,65 +9,55 @@ namespace GestorPasswordsTest
     {
         private Category aCategory;
         private User aUser;
+        private CreditCard aCreditCard;
 
         [TestInitialize]
         public void Initialize()
         {
-            aUser = new User();
+            aUser = new User()
+            {
+                MasterPassword = "password",
+                Name = "UserName"
+            };
             aCategory = new Category()
             {
                 User = aUser,
                 Name = "Category"
             };
-            aUser.AddCategory(aCategory);
-        }
-
-        [TestMethod]
-        public void CreditCardToString()
-        {
-            CreditCard aCreditCard = new CreditCard()
-            {
-                Number = "1234567891234567",
-                Type = "Visa",
-                Name = "Visa Gold",
-                Code = "234",
-                Notes = "",
-                ExpirationDate = new DateTime(2023, 12, 25),
-                Category = aCategory,
-            };
-            Assert.AreEqual(aCreditCard.ToString(), "[Visa Gold] [Visa] [1234 5678 9123 4567]");
-        }
-        
-        [TestMethod]
-        public void FormatCreditCardNumber()
-        {
-            string creditCardNumber = "1234567891234567";
-            Assert.AreEqual("1234 5678 9123 4567", CreditCard.FormatNumber(creditCardNumber));
-        }
-
-        [TestMethod]
-        public void HideCreditCardNumber()
-        {
-            Category aCategory = new Category()
-            {
-                Name = "Category",
-                User = new User() { MasterPassword = "password", Name = "UserName" }
-            };
-            CreditCard creditCard = new CreditCard()
+            aCreditCard = new CreditCard()
             {
                 Number = "1234567891234567",
                 Type = "Visa Gold",
                 Name = "Visa",
                 Code = "123",
                 Notes = "",
-                ExpirationDate = DateTime.Now,
+                ExpirationDate = new DateTime(2023, 12, 25),
                 Category = aCategory
             };
-            Assert.AreEqual("XXXX XXXX XXXX 4567", creditCard.HideNumber);
+
+            aUser.AddCategory(aCategory);
+        }
+
+        [TestMethod]
+        public void CreditCardToString()
+        {
+            Assert.AreEqual(aCreditCard.ToString(), "[Visa] [Visa Gold] [1234 5678 9123 4567]");
+        }
+
+        [TestMethod]
+        public void FormatCreditCardNumber()
+        {
+            var creditCardNumber = "1234567891234567";
+            Assert.AreEqual("1234 5678 9123 4567", CreditCard.FormatNumber(creditCardNumber));
+        }
+
+        [TestMethod]
+        public void HideCreditCardNumber()
+        {
+            Assert.AreEqual("XXXX XXXX XXXX 4567", aCreditCard.HideNumber);
         }
     }
 }
 
 
 
-        
