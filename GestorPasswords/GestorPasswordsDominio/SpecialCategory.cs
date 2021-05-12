bee39015738c0154ca87f48063d0f44cdddd21c1
Fernanda <secinarof@gmail.com>
2021-05-11 23:38:15 -0300

    changed hashtable for a dictionary in password manager

--- GestorPasswords/GestorPasswordsDominio/SpecialCategory.cs
+++ GestorPasswords/GestorPasswordsDominio/SpecialCategory.cs
@@ -10,18 +10,11 @@
         public override bool AddUserPasswordPair(UserPasswordPair aUserPasswordPair)
         {
             AddUserPasswordPairToHashTable(aUserPasswordPair);
-            
             return true;
         }
 
         public override bool RemoveUserPasswordPair(UserPasswordPair aUserPasswordPair)
         {
-            if (!this.UserPasswordPairAlredyExistsInCategory(aUserPasswordPair.Username, aUserPasswordPair.Site))
-            {
-                string message = $"The user-password pair ({aUserPasswordPair.Username}-{aUserPasswordPair.Site}) does not exist in {this.Name}";
-                throw new ExceptionUserPasswordPairDoesNotExist(message);
-            }
-
             RemoveUserPasswordPairFromCollection(aUserPasswordPair);
             return true;
         }
