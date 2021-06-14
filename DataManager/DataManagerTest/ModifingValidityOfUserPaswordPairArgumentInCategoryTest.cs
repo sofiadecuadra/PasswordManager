f24af1caf421ac7e7588b0ce04976c9430ed2553
Fernanda <secinarof@gmail.com>
2021-06-13 21:12:50 -0300

    red, green and refactor for test of modifying passwords

--- DataManager/DataManagerTest/ModifingValidityOfUserPaswordPairArgumentInCategoryTest.cs
+++ DataManager/DataManagerTest/ModifingValidityOfUserPaswordPairArgumentInCategoryTest.cs
@@ -7,13 +7,20 @@ namespace DataManagerTest
     [TestClass]
     public class ModifingValidityOfUserPaswordPairArgumentInCategoryTest
     {
+        private DataManager DataManager;
         private Category aCategory;
         private User aUser;
 
         [TestInitialize]
         public void Initialize()
         {
-            aUser = new User();
+            DataManager = new DataManager();
+            aUser = new User()
+            {
+                Username = "Fernanda",
+                MasterPassword = "password",
+            };
+            DataManager.AddUser(aUser);
             aCategory = new Category()
             {
                 User = aUser,
@@ -327,6 +334,18 @@ namespace DataManagerTest
             aCategory.AddUserPasswordPair(anotherUserPasswordPair);
 
             _ = aCategory.ModifyUserPasswordPair(aUserPasswordPair, newUserPasswordPair);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            using (var dbContext = new DataManagerContext())
+            {
+                dbContext.Users.RemoveRange(dbContext.Users);
+                dbContext.Categories.RemoveRange(dbContext.Categories);
+                dbContext.UserPasswordPairs.RemoveRange(dbContext.UserPasswordPairs);
+                dbContext.SaveChanges();
+            }
         }
     }
 }
