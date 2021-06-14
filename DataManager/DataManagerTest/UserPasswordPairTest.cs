52ff41f02630b9c2a2c4489f1420897930570618
Fernanda <secinarof@gmail.com>
2021-06-13 21:19:36 -0300

    red, green and refactor for tests of data of password

--- DataManager/DataManagerTest/UserPasswordPairTest.cs
+++ DataManager/DataManagerTest/UserPasswordPairTest.cs
@@ -9,11 +9,18 @@ namespace DataManagerTest
     {
         private Category aCategory;
         private User aUser;
+        private DataManager DataManager;
 
         [TestInitialize]
         public void Initialize()
         {
-            aUser = new User();
+            DataManager = new DataManager();
+            aUser = new User()
+            {
+                Username = "Fernanda",
+                MasterPassword = "password",
+            };
+            DataManager.AddUser(aUser);
             aCategory = new Category()
             {
                 User = aUser,
@@ -48,8 +55,20 @@ namespace DataManagerTest
                 Site = "mySite",
             };
 
-            Assert.AreEqual(DateTime.Now.ToString("yyyyMMddhhmmss"), 
+            Assert.AreEqual(DateTime.Now.ToString("yyyyMMddhhmmss"),
                 aUserPasswordPair.LastModifiedDate.ToString("yyyyMMddhhmmss"));
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            using (var dbContext = new DataManagerContext())
+            {
+                dbContext.Users.RemoveRange(dbContext.Users);
+                dbContext.Categories.RemoveRange(dbContext.Categories);
+                dbContext.CreditCards.RemoveRange(dbContext.CreditCards);
+                dbContext.SaveChanges();
+            }
         }
     }
 }
