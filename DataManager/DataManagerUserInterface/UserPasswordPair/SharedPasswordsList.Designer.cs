1439cf89385cd6b2aac783549738bf3c5a8ee46d
Fernanda <secinarof@gmail.com>
2021-06-16 14:18:10 -0300

    beginning of shared passwords

---
+++ DataManager/DataManagerUserInterface/UserPasswordPair/SharedPasswordsList.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace PasswordsManagerUserInterface
+{
+    partial class SharedPasswordsList
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSharedPasswords = new System.Windows.Forms.DataGridView();
+            this.lblSharedPasswords = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnShare = new System.Windows.Forms.Button();
+            this.btnUnshare = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSharedPasswords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSharedPasswords
+            // 
+            this.dgvSharedPasswords.AllowUserToAddRows = false;
+            this.dgvSharedPasswords.AllowUserToDeleteRows = false;
+            this.dgvSharedPasswords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSharedPasswords.Location = new System.Drawing.Point(55, 150);
+            this.dgvSharedPasswords.Name = "dgvSharedPasswords";
+            this.dgvSharedPasswords.ReadOnly = true;
+            this.dgvSharedPasswords.Size = new System.Drawing.Size(700, 177);
+            this.dgvSharedPasswords.TabIndex = 0;
+            // 
+            // lblSharedPasswords
+            // 
+            this.lblSharedPasswords.AutoSize = true;
+            this.lblSharedPasswords.Location = new System.Drawing.Point(55, 110);
+            this.lblSharedPasswords.Name = "lblSharedPasswords";
+            this.lblSharedPasswords.Size = new System.Drawing.Size(98, 13);
+            this.lblSharedPasswords.TabIndex = 1;
+            this.lblSharedPasswords.Text = "Shared Passwords:";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(672, 24);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnShare
+            // 
+            this.btnShare.Location = new System.Drawing.Point(266, 376);
+            this.btnShare.Name = "btnShare";
+            this.btnShare.Size = new System.Drawing.Size(75, 23);
+            this.btnShare.TabIndex = 3;
+            this.btnShare.Text = "Share";
+            this.btnShare.UseVisualStyleBackColor = true;
+            this.btnShare.Click += new System.EventHandler(this.btnShare_Click);
+            // 
+            // btnUnshare
+            // 
+            this.btnUnshare.Location = new System.Drawing.Point(476, 376);
+            this.btnUnshare.Name = "btnUnshare";
+            this.btnUnshare.Size = new System.Drawing.Size(75, 23);
+            this.btnUnshare.TabIndex = 4;
+            this.btnUnshare.Text = "Unshare";
+            this.btnUnshare.UseVisualStyleBackColor = true;
+            // 
+            // SharedPasswordsList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnUnshare);
+            this.Controls.Add(this.btnShare);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lblSharedPasswords);
+            this.Controls.Add(this.dgvSharedPasswords);
+            this.Name = "SharedPasswordsList";
+            this.Size = new System.Drawing.Size(804, 451);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSharedPasswords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSharedPasswords;
+        private System.Windows.Forms.Label lblSharedPasswords;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnShare;
+        private System.Windows.Forms.Button btnUnshare;
+    }
+}
---
+++ DataManager/DataManagerUserInterface/UserPasswordPair/SharedPasswordsList.cs
@@ -0,0 +1,104 @@
+﻿using DataManagerDomain;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PasswordsManagerUserInterface
+{
+    public partial class SharedPasswordsList : UserControl
+    {
+        public DataManager PasswordManager { get; private set; }
+        public Panel PnlMainWindow { get; private set; }
+        private readonly DataGridViewButtonColumn fullViewSharedPasswords;
+
+        public SharedPasswordsList(DataManager aPasswordManager, Panel panel)
+        {
+            InitializeComponent();
+            PasswordManager = aPasswordManager;
+            PnlMainWindow = panel;
+            fullViewSharedPasswords = new DataGridViewButtonColumn();
+            LoadSharedPasswords();
+        }
+
+        private void LoadSharedPasswords()
+        {
+            var userPasswordPairs = PasswordManager.
+                CurrentUser.GetSharedUserPasswordPairs();
+            LoadDataGridSharedPasswords(userPasswordPairs);
+        }
+
+        private void LoadDataGridSharedPasswords(UserPasswordPair[] userPasswordPairs)
+        {
+            SetColumnsQuantity();
+            SetCategoryColumn();
+            SetSiteColumn();
+            SetUsernameColumn();
+            SetLastModifiedColumn();
+            SetFullViewColumn();
+            dgvSharedPasswords.DataSource = userPasswordPairs;
+            ChangeWidthWhenScrollBarIsVisible();
+        }
+
+        private void SetColumnsQuantity()
+        {
+            dgvSharedPasswords.AutoGenerateColumns = false;
+            dgvSharedPasswords.ColumnCount = 4;
+        }
+
+        private void SetCategoryColumn()
+        {
+            dgvSharedPasswords.Columns[0].Name = "Category";
+            dgvSharedPasswords.Columns[0].HeaderText = "Category";
+            dgvSharedPasswords.Columns[0].DataPropertyName = "Category";
+            dgvSharedPasswords.Columns[0].Width = 135;
+        }
+
+        private void SetSiteColumn()
+        {
+            dgvSharedPasswords.Columns[1].Name = "Site";
+            dgvSharedPasswords.Columns[1].HeaderText = "Site";
+            dgvSharedPasswords.Columns[1].DataPropertyName = "Site";
+            dgvSharedPasswords.Columns[1].Width = 215;
+        }
+
+        private void SetUsernameColumn()
+        {
+            dgvSharedPasswords.Columns[2].Name = "User";
+            dgvSharedPasswords.Columns[2].HeaderText = "User";
+            dgvSharedPasswords.Columns[2].DataPropertyName = "Username";
+            dgvSharedPasswords.Columns[2].Width = 215;
+        }
+
+        private void SetLastModifiedColumn()
+        {
+            dgvSharedPasswords.Columns[3].Name = "LastModified";
+            dgvSharedPasswords.Columns[3].HeaderText = "Last Modified";
+            dgvSharedPasswords.Columns[3].DataPropertyName = "LastModifiedDate";
+            dgvSharedPasswords.Columns[3].DefaultCellStyle.Format = "dd/MM/yyyy";
+            dgvSharedPasswords.Columns[3].Width = 70;
+        }
+
+        private void SetFullViewColumn()
+        {
+            dgvSharedPasswords.Columns.Add(fullViewSharedPasswords);
+            fullViewSharedPasswords.HeaderText = @"";
+            fullViewSharedPasswords.Name = "Full view";
+            fullViewSharedPasswords.Text = "Full view";
+            fullViewSharedPasswords.Width = 61;
+            fullViewSharedPasswords.UseColumnTextForButtonValue = true;
+        }
+
+        private void ChangeWidthWhenScrollBarIsVisible()
+        {
+            if (dgvSharedPasswords.Controls.OfType<ScrollBar>().Last().Visible)
+            {
+                dgvSharedPasswords.Width = 717;
+            }
+        }
+
+        private void btnShare_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
